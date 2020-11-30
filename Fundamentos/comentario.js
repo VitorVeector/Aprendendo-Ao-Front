4f7a12b67655f6a58807877a67c209961a167f62
Vitor Hugo <67335644+VitorVeector@users.noreply.github.com>
2020-11-30 15:29:57 -0300

    Create comentario.js

---
+++ Fundamentos/comentario.js
@@ -0,0 +1,13 @@
+/*Tipo de comentário 1.
+Pode haver quebra de linha */
+
+//Tipo de comentário 2. Está esclusivamente reservado para uma Linha
+
+/*
+ * Tipo de comentário 3.
+ * Geralmente utilizado para listas
+ * 1
+ * 2
+ * 3
+ * 4
+ */
