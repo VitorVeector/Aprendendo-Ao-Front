6bad64ab3987f45c15170ceb2d8af01313de178e
Vitor Hugo <67335644+VitorVeector@users.noreply.github.com>
2020-12-03 18:36:58 -0300

    Finish Math, started string

---
+++ Fundamentos/String.js
@@ -0,0 +1,23 @@
+const escola = "Alf4Master";
+
+//Irá me retornar o que há no array 2 da variavel escola, lembrando que sempre começa contando do zero
+console.log(escola.charAt(2));
+
+//Mostrará a localização do caractere na tabela unicode
+console.log(escola.charCodeAt(2))
+
+// Irá verificar se existe tal caracter dentro da String
+console.log(escola.indexOf(4));
+
+// Irá imprimir o valor a apartir do array selecionado
+console.log(escola.substring(2));
+console.log(escola.substring(2, 6));
+
+//Dividir elementos em string
+console.log('Marcio,José,PedrinhoPVP'.split(','));
+
+
+
+
+
+
--- Fundamentos/math.js
+++ Fundamentos/math.js
@@ -1,6 +1,21 @@
 const raio = 5.6;
 const area = Math.PI = Math.pow(raio, 2);
 
+
+
 console.log(area);
 
 console.log(typeof Math);
+
+
+//Potência em JS
+var x = Math.pow(5, 2);
+console.log(x);
+
+//PI
+var pi = Math.PI;
+console.log(pi);
+
+
+
+
