ae5ea8e27696bcdb4d90cf7267aa71b1f58f9294
Vitor Hugo <67335644+VitorVeector@users.noreply.github.com>
2020-12-04 16:23:57 -0300

    Update numberWarning.js

--- Fundamentos/numberWarning.js
+++ Fundamentos/numberWarning.js
@@ -4,9 +4,11 @@ console.log(7 / 0);
 // O 10 entre aspas é do tipo string, em outras linguagem o trecho abaixo daria um erro. JS verifica o que tem dentro da String
 console.log("10" / 2);
 
+//Mas há excessões, em adição, ele ADICIONARA o 2 não como soma, ou seja, concatenação
+console.log("10" + 2);
+
 // O Resultado correto seria 0.8 em outras linguagens, porém as especificações em Js são diferentes. Isso só acontece em expressões que não retornam um número inteiro
 console.log(0.1 + 0.7); 
-
 console.log( 0.3 + 0.7);
 
 //Para impremir 2 casas decimais após um número inteiro, além de arredondar
