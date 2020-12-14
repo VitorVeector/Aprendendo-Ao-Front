160b6a26fb191eb1387f3897fb3d90eda15d09a4
Vitor Hugo <67335644+VitorVeector@users.noreply.github.com>
2020-12-14 10:45:46 -0300

    Create atribuicao.js

---
+++ Fundamentos/atribuicao.js
@@ -0,0 +1,29 @@
+const num = 7
+let num2 = 3
+
+//Atribuindo o valor de num no num2
+num2 += num // num2 = num2 + num
+console.log(num2)
+
+console.log()
+
+//Subtraindo algum valor de num2
+num2 -= 1 // num2 = num2 - 1
+console.log(num2)
+
+console.log()
+
+num2 *= 3 // num2 = num2 * 3
+console.log(num2)
+
+console.log()
+
+num2 /= 3 // num2 = num2 / 3
+console.log(num2)
+
+console.log()
+
+num2 %= 2 // num2 = num2 & 2
+console.log(num2)
+//Dica? se for um valor par o valor sempre sera 0, e se for impar sempre sera 1
+
