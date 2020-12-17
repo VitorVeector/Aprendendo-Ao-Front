a6117c3318c04412d2b39f74ecd414297bf2d345
Vitor Hugo <67335644+VitorVeector@users.noreply.github.com>
2020-12-17 08:39:51 -0300

    revisao de operadores

--- Fundamentos/destructuring4.js
+++ Fundamentos/destructuring4.js
@@ -1,7 +1,10 @@
-function rand({min = 0, max = 1000}){
+function rand([min = 0, max = 1000]){
     if(min > max) [min, max] = [max, min]
     const valor = Math.random() * (max - min) + min
     return Math.floor(valor)
 }
 
 console.log(rand([50, 40]))
+console.log(rand([998])) //Valor minimo
+console.log(rand([, 10])) //Valor maximo
+console.log(rand([])) 
