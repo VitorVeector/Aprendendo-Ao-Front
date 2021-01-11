74c29e0a5a300569e1d27bc688248312a3570107
Vitor Hugo <67335644+VitorVeector@users.noreply.github.com>
2021-01-11 10:51:47 -0300

    Estrutura de controle

---
+++ Estruturas de controle/if1.js
@@ -0,0 +1,25 @@
+console.log('Expressão com condicional:')
+function resultado(nota) {
+    if(nota >= 7){
+        console.log('Aprovado com a nota: ' + nota)
+    }else{
+        console.log('Reprovado com a nota: ' + nota)
+    }
+}
+
+resultado(6)
+
+console.log()
+console.log('Expressão booleana:')
+function seForVddEuDigo(valor){
+    if(valor){
+        console.log('Eh vdd')
+    }
+}
+
+seForVddEuDigo()
+seForVddEuDigo(0)
+seForVddEuDigo(null)
+seForVddEuDigo(undefined)
+seForVddEuDigo(NaN)
+seForVddEuDigo('')
