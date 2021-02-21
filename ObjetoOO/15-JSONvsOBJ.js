81ad7e0deb425dcc1c87c6ee42732b721c3d2525
VitorVeector <job_vitoraraujo@outlook.com>
2021-02-21 09:27:31 -0300

    JSON vs OBJECT
    Conceito de JSON, e sua diferenciação a OBJETO

--- ObjetoOO/15-JSONvsOBJ.js
+++ ObjetoOO/15-JSONvsOBJ.js
@@ -4,3 +4,4 @@ console.log(JSON.stringify(obj)) // me retorna em String
 //console.log(JSON.parse("{ 'a': 1, 'b': 2, 'c': 3}")) TEM QUE SER POR ASPAS DUPLAS
 console.log(JSON.parse('{ "a": 1, "b": 2, "c": 3}')) // me retorna em OBJ
 console.log(JSON.parse('{ "a": 1, "b": "string", "c": true, "d": {}, "e": []}'))
+
